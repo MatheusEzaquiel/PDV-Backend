ff41c959561f704218871ddabc7fd8718b1ed27f
matheus <matheusbezaquiel@gmail.com>
2025-07-26 21:26:03 -0300

    refactor(Product): Add price attribute to DTO

--- src/main/java/com/mbe/viapdv/model/product/dto/BasicProductDTO.java
+++ src/main/java/com/mbe/viapdv/model/product/dto/BasicProductDTO.java
@@ -3,10 +3,17 @@ package com.mbe.viapdv.model.product.dto;
 import com.mbe.viapdv.model.category.dto.ListCategoryDTO;
 import com.mbe.viapdv.model.product.Product;
 
-public record BasicProductDTO(long id, String name, String sku, ListCategoryDTO category) {
+import java.math.BigDecimal;
+
+public record BasicProductDTO(long id, String name, String sku, ListCategoryDTO category, BigDecimal price) {
     
     public BasicProductDTO(Product product) {
-        this(product.getId(), product.getName(), product.getSku(),
-             product.getCategory() != null ? new ListCategoryDTO(product.getCategory()) : null);
+        this(
+                product.getId(),
+                product.getName(),
+                product.getSku(),
+                (product.getCategory() != null ? new ListCategoryDTO(product.getCategory()) : null),
+                product.getPrice()
+        );
     }
 }
